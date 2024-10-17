1873900aa3475ee6212a4110cfe72b339352cc5a
Tri <dongtri567@gmail.com>
2024-10-17 16:27:16 +0700

    #

---
+++ Admindanhmuc.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Title="Danh mục" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="Admindanhmuc.aspx.cs" Inherits="BTLBlog.Admindanhmuc" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+     <div class="ad-container-dm">
+     <asp:MultiView ID="mvDanhmuc" runat="server" ActiveViewIndex="0">
+         <asp:View ID="ViewDanhSach" runat="server">
+             <div class="row">
+                <div class="col-md-6">
+                     <asp:GridView ID="gvLoaiDanhmuc" runat="server" AutoGenerateColumns="false" CssClass="table table-striped" OnSelectedIndexChanged="gvLoaiDanhmuc_SelectedIndexChanged" DataKeyNames="LoaiDanhmucId">
+                        <Columns>
+                            <asp:TemplateField HeaderText="STT">
+                                <ItemTemplate>
+                                    <%# Container.DataItemIndex + 1 %> 
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:BoundField DataField="TenLoai" HeaderText="Tên Loại" />
+                            <asp:ButtonField Text="Chọn" CommandName="Select" ButtonType="Button" />
+                        </Columns>
+                    </asp:GridView>
+                </div>
+                 <div class="col-md-6">
+                     <asp:GridView ID="gvDanhmuc" runat="server" AutoGenerateColumns="false" CssClass="table table-striped ad-table-data" OnRowCommand="gvDanhmuc_RowCommand">
+                         <Columns>
+                             <asp:TemplateField HeaderText="STT">
+                                <ItemTemplate>
+                                    <%# Container.DataItemIndex + 1 %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                             <asp:BoundField DataField="TenDanhmuc" HeaderText="Tên Loại" />
+                             <asp:BoundField DataField="MaDanhmuc" HeaderText="Mã Loại" />
+                             <asp:TemplateField HeaderText="Danh mục cha">
+                                <ItemTemplate>
+                                    <%# Eval("TenDanhmucCha") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                             <asp:TemplateField>
+                                 <ItemTemplate>
+                                     <asp:Button ID="btnSua" runat="server" Text="Sửa" CommandName="Sua" CommandArgument='<%# Eval("DanhmucId") %>' />
+                                     <asp:Button ID="btnXoa" runat="server" Text="Xóa" CommandName="Xoa" CommandArgument='<%# Eval("DanhmucId") %>' />
+                                 </ItemTemplate>
+                             </asp:TemplateField>
+                         </Columns>
+                     </asp:GridView>
+                    <asp:Button ID="btnThemMoi" runat="server" Text="Thêm mới" OnClick="btnThemMoi_Click" />
+                </div>
+            </div>
+         </asp:View>
+         <asp:View ID="ViewThemMoi" runat="server">
+             <h2>Thêm mới loại danh mục</h2>
+             <label>Tên loại</label>
+             <asp:TextBox ID="txtTenLoai" runat="server"/>
+             <asp:RequiredFieldValidator ControlToValidate="txtTenLoai" ErrorMessage="Tên loại không được để trống" runat="server" />
+             <br />
+             <label>Mã loại</label>
+             <asp:TextBox ID="txtMaLoai" runat="server" />
+             <asp:RequiredFieldValidator ControlToValidate="txtMaLoai" ErrorMessage="Mã loại không được để trống" runat="server" />
+             <br />
+             <label>Chọn Danh mục cha</label>
+            <asp:DropDownList ID="ddlLoaiDanhmucCha" runat="server" DataTextField="TenLoai" DataValueField="LoaiDanhmucId" AppendDataBoundItems="true">
+            </asp:DropDownList>
+            <asp:RequiredFieldValidator ControlToValidate="ddlLoaiDanhmucCha" InitialValue="" ErrorMessage="Vui lòng chọn danh mục cha" runat="server" />
+            <br />
+             <asp:Button ID="btnLuuMoi" runat="server" Text="Lưu" OnClick="btnLuuMoi_Click" />
+             <asp:Button ID="btnHuyMoi" runat="server" Text="Hủy" OnClick="btnHuyMoi_Click" />
+         </asp:View>
+         <asp:View ID="ViewSua" runat="server">
+             <h2>Sửa Loại danh mục</h2>
+             
+             <asp:HiddenField ID="hfDanhmucId" runat="server" />
+             <asp:Label ID="lblSuaTenLoai" runat="server" Text="Tên Loại: "></asp:Label>
+             <asp:TextBox ID="txtSuaTenLoai" runat="server"></asp:TextBox>
+             <asp:RequiredFieldValidator ControlToValidate="txtSuaTenLoai" ErrorMessage="Tên loại không được để trống" runat="server" />
+             <br />
+             <asp:Label ID="lblSuaMaLoai" runat="server" Text="Mã Loại: "></asp:Label>
+             <asp:TextBox ID="txtSuaMaLoai" runat="server"></asp:TextBox>
+             <asp:RequiredFieldValidator ControlToValidate="txtSuaMaLoai" ErrorMessage="Mã loại không được để trống" runat="server" />
+             <br />
+             <label>Chọn Danh mục cha</label>
+            <asp:DropDownList ID="ddlSuaLoaiDanhmucCha" runat="server" DataTextField="TenLoai" DataValueField="LoaiDanhmucId" AppendDataBoundItems="true">
+            </asp:DropDownList>
+            <asp:RequiredFieldValidator ControlToValidate="ddlSuaLoaiDanhmucCha" InitialValue="" ErrorMessage="Vui lòng chọn danh mục cha" runat="server" />
+            <br />
+             <asp:Button ID="btnCapNhat" runat="server" Text="Cập nhật" OnClick="btnCapNhat_Click" />
+             <asp:Button ID="btnHuySua" runat="server" Text="Hủy" OnClick="btnHuyMoi_Click" />
+         </asp:View>
+     </asp:MultiView>
+  </div>
+</asp:Content>
---
+++ Admindanhmuc.aspx.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BTLBlog
+{
+    public partial class Admindanhmuc : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+                LoadLoaiDanhmucCha();
+            }
+        }
+        private void LoadLoaiDanhmucCha()
+        {
+            using (var context = new BlogDBEntities())
+            {
+                var loaiDanhmucs = context.LoaiDanhmucs.ToList();
+
+                ddlLoaiDanhmucCha.DataSource = loaiDanhmucs;
+                ddlLoaiDanhmucCha.DataBind();
+
+                ddlSuaLoaiDanhmucCha.DataSource = loaiDanhmucs;
+                ddlSuaLoaiDanhmucCha.DataBind();
+            }
+        }
+        private void LoadData()
+        {
+            using (var context = new BlogDBEntities())
+            {
+                var data = from d in context.Danhmucs
+                           join ld in context.LoaiDanhmucs on d.IdLoaiDanhmuc equals ld.LoaiDanhmucId into danhMucLoai
+                           from ld in danhMucLoai.DefaultIfEmpty()
+                           select new
+                           {
+                               d.DanhmucId,
+                               d.TenDanhmuc,
+                               d.MaDanhmuc,
+                               TenDanhmucCha = ld != null ? ld.TenLoai : "Không có danh mục cha"
+                           };
+
+                gvDanhmuc.DataSource = data.ToList();
+                gvDanhmuc.DataBind();
+
+
+                var loaiDanhmucs = context.LoaiDanhmucs.ToList();
+                gvLoaiDanhmuc.DataSource = loaiDanhmucs;
+                gvLoaiDanhmuc.DataBind();
+
+                mvDanhmuc.ActiveViewIndex = 0;
+            }
+        }
+        protected void gvLoaiDanhmuc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedLoaiDanhmucId = Convert.ToInt32(gvLoaiDanhmuc.SelectedDataKey.Value);
+
+            LoadDanhmucByLoaiDanhmucId(selectedLoaiDanhmucId);
+        }
+        private void LoadDanhmucByLoaiDanhmucId(int loaiDanhmucId)
+        {
+            using (var context = new BlogDBEntities())
+            {
+                var data = from d in context.Danhmucs
+                           where d.IdLoaiDanhmuc == loaiDanhmucId
+                           join ld in context.LoaiDanhmucs on d.IdLoaiDanhmuc equals ld.LoaiDanhmucId into danhMucLoai
+                           from ld in danhMucLoai.DefaultIfEmpty()
+                           select new
+                           {
+                               d.DanhmucId,
+                               d.TenDanhmuc,
+                               d.MaDanhmuc,
+                               TenDanhmucCha = ld != null ? ld.TenLoai : "Không có danh mục cha"
+                           };
+
+                gvDanhmuc.DataSource = data.ToList();
+                gvDanhmuc.DataBind();
+            }
+        }
+        protected void gvDanhmuc_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Sua")
+            {
+                int id = Convert.ToInt32(e.CommandArgument);
+                using (var context = new BlogDBEntities())
+                {
+                    var danhmuc = context.Danhmucs.SingleOrDefault(d => d.DanhmucId == id);
+                    if (danhmuc != null)
+                    {
+                        txtSuaTenLoai.Text = danhmuc.TenDanhmuc;
+                        txtSuaMaLoai.Text = danhmuc.MaDanhmuc;
+                        ddlSuaLoaiDanhmucCha.SelectedValue = danhmuc.IdLoaiDanhmuc.ToString();
+
+                        // Chuyển đến View chỉnh sửa
+                        mvDanhmuc.ActiveViewIndex = 1; // Giả sử View chỉnh sửa là View index 1
+                    }
+                }
+
+                mvDanhmuc.ActiveViewIndex = 2;
+            }
+            if (e.CommandName == "Xoa")
+            {
+                int id = Convert.ToInt32(e.CommandArgument);
+                using (var context = new BlogDBEntities())
+                {
+                    var Danhmuc = context.Danhmucs.SingleOrDefault(ld => ld.DanhmucId == id);
+                    if (Danhmuc != null)
+                    {
+                        context.Danhmucs.Remove(Danhmuc);
+                        context.SaveChanges();
+                    }
+                }
+                LoadData();
+            }
+        }
+        protected void btnThemMoi_Click(object sender, EventArgs e)
+        {
+            mvDanhmuc.ActiveViewIndex = 1;
+        }
+
+        protected void btnHuyMoi_Click(object sender, EventArgs e)
+        {
+            mvDanhmuc.ActiveViewIndex = 0;
+        }
+        protected void btnLuuMoi_Click(object sender, EventArgs e)
+        {
+            using (var context = new BlogDBEntities())
+            {
+                var Danhmuc = new Danhmuc
+                {
+                    TenDanhmuc = txtTenLoai.Text,
+                    MaDanhmuc = txtMaLoai.Text,
+                    IdLoaiDanhmuc = int.Parse(ddlLoaiDanhmucCha.SelectedValue)
+                };
+
+                context.Danhmucs.Add(Danhmuc);
+                context.SaveChanges();
+
+                // Cập nhật lại GridView
+                LoadData();
+
+                // Chuyển về View danh sách
+                mvDanhmuc.ActiveViewIndex = 0;
+            }
+        }
+        protected void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            using (var context = new BlogDBEntities())
+            {
+                int id = Convert.ToInt32(hfDanhmucId.Value);
+
+                var danhmuc = context.Danhmucs.SingleOrDefault(d => d.DanhmucId == id);
+
+                if (danhmuc != null)
+                {
+                    // Cập nhật thông tin danh mục
+                    danhmuc.TenDanhmuc = txtTenLoai.Text;
+                    danhmuc.MaDanhmuc = txtMaLoai.Text;
+                    danhmuc.IdLoaiDanhmuc = int.Parse(ddlLoaiDanhmucCha.SelectedValue);
+
+                    context.SaveChanges();
+
+                    // Cập nhật lại GridView
+                    LoadData();
+
+                    // Hiển thị thông báo thành công
+                    string script = "<script>Custom.Mytoast('Cập nhật thành công!', '/images/success.svg');</script>";
+                    ClientScript.RegisterStartupScript(this.GetType(), "ShowToast", script);
+
+                    // Chuyển về View danh sách
+                    mvDanhmuc.ActiveViewIndex = 0;
+                }
+                else
+                {
+                    // Trường hợp danh mục không tồn tại
+                    string script = "<script>Custom.Mytoast('Danh mục không tồn tại!', '/images/error.svg');</script>";
+                    ClientScript.RegisterStartupScript(this.GetType(), "ShowToast", script);
+                }
+            }
+        }
+    }
+}
